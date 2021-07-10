5ca09af7b478bfe1ae0bf7fecde71de5add30d8c
Saurabh Joshi <joshisaurabh2610@gmail.com>
2021-07-10 20:31:24 +0530

    Startup Files

---
+++ script.js
@@ -0,0 +1,48 @@
+var arr = [[], [], [], [], [], [], [], [], []]
+
+for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+        arr[i][j] = document.getElementById(i * 9 + j);
+
+    }
+}
+
+
+var board = [[], [], [], [], [], [], [], [], []]
+
+function FillBoard(board) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (board[i][j] != 0) {
+                arr[i][j].innerText = board[i][j]
+            }
+
+            else
+                arr[i][j].innerText = ''
+        }
+    }
+}
+
+let GetPuzzle = document.getElementById('GetPuzzle')
+let SolvePuzzle = document.getElementById('SolvePuzzle')
+
+GetPuzzle.onclick = function () {
+    var xhrRequest = new XMLHttpRequest()
+    xhrRequest.onload = function () {
+        var response = JSON.parse(xhrRequest.response)
+        console.log(response)
+        board = response.board
+        FillBoard(board)
+    }
+    xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy')
+    //we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
+    xhrRequest.send()
+}
+
+SolvePuzzle.onclick = () => {
+    SudokuSolver(board, 0, 0);
+};
+
+function SudokuSolver(board, i, j) {
+    // Write your Code here
+}
