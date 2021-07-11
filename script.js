fbb92cc55c393a041a559700277c14c3bad2c2d5
Saurabh Joshi <joshisaurabh2610@gmail.com>
2021-07-11 06:30:32 +0530

    Sudoku_Solver_Completed

--- script.js
+++ script.js
@@ -40,9 +40,66 @@ GetPuzzle.onclick = function () {
 }
 
 SolvePuzzle.onclick = () => {
-    SudokuSolver(board, 0, 0);
+    SudokuSolver(board, 0, 0, 9);
 };
 
-function SudokuSolver(board, i, j) {
-    // Write your Code here
+function isValid(board, i, j, num, n) {
+	// Row and Col Checks
+	for (let x = 0; x < n; x++) {
+		if (board[i][x] == num || board[x][j] == num) {
+			return false;
+		}
+	}
+
+	// SubMatrix Check
+	let rn = Math.sqrt(n);
+	let si = i - i % rn;
+	let sj = j - j % rn;
+
+	for (let x = si; x < si + rn; x++) {
+		for (let y = sj; y < sj + rn; y++) {
+			if(board[x][y]==num){
+				return false;
+			}
+		}
+	}
+
+	return true;
+
+}
+
+function SudokuSolver(board, i, j, n) {
+	// Base Case
+	if (i == n) {
+		// Print(board, n);
+        FillBoard(board)
+		return true;
+	}
+
+	// if we are not inside the board
+	if (j == n) {
+		return SudokuSolver(board, i + 1, 0, n);
+	}
+
+	// if cell is already filled -> just move ahead
+	if (board[i][j] != 0) {
+		return SudokuSolver(board, i, j + 1, n);
+	}
+
+	//we try to fill the cell with an appropriate number
+
+	for (let num = 1; num <= 9; num++) {
+		// Check is num can be filled
+		if (isValid(board, i, j, num, n)) {
+			board[i][j] = num;
+			let subAns = SudokuSolver(board, i, j + 1, n);
+			if (subAns) {
+				return true;
+			}
+			// Backtracking -> undo the change
+			board[i][j] = 0;
+		}
+	}
+
+	return false;
 }
